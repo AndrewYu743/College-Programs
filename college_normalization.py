943d1c466b2a5280a749c508cc9ebcae099a25dd
williamyzhang <80303545+williamyzhang@users.noreply.github.com>
2022-01-03 15:55:34 -0600

    added Demographics & Program Percentages

--- college_normalization.py
+++ college_normalization.py
@@ -64,13 +64,49 @@ def make_Universities(all_data_elements):
     print("\tMaking 'Universities' table")
 
     Universities = all_data_elements[['UNITID', 'INSTNM', 'CITY', 'STABBR', 'INSTURL',
-                                      'NUMBRANCH', 'CONTROL', 'ADM_RATE']]
+                                      'NUMBRANCH', 'CONTROL', 'ADM_RATE','CURROPER']]
 
-    # Universities = Universities.rename(columns={
-    #    'UNITID':'unit_id'
-    # })
+    Universities = Universities.rename(columns={
+        'UNITID':'unit_id',
+        'CURROPER':'Currently Operating'
+     })
 
     Universities.to_csv('Universities.csv', index=False, na_rep=r'\N')
+
+def make_Demographics(all_data_elements):
+    print("\tMaking 'Demographics' table")
+
+    Demographics = all_data_elements[['UNITID','UGDS','UG','PAR_ED_PCT_1STGEN','AGE_ENTRY',
+                                      'AGEGE24','FEMALE','MD_FAMINC','MEDIAN_HH_INC']]
+
+    Demographics = Demographics.rename(columns={
+        'UGDS':'Undergraduate Degree-seeking Enrollment',
+        'UG':'Total Undergraduate Enrollment',
+        'PAR_ED_PCT_1STGEN':'Percentage First Generation',
+        'AGE_ENTRY':'Avg Age of Entry',
+        'AGEGE24':'Percent over 23 at Entry',
+        'FEMALE':'Share of Female Students',
+        'MD_FAMINC': 'Median Family Income',
+        'MEDIAN_HH_INC': 'Median Household Income'
+
+    })
+
+    Demographics.to_csv('Demographics.csv', index=False, na_rep=r'\N')
+
+def make_Program_Percentages(all_data_elements):
+    print("\tMaking 'Program Percentages' table")
+
+    #there's several CIP subjects skipped
+    skipped = [2, 6, 7, 8, 17, 18, 20, 21, 28, 32, 33, 34, 35, 36, 37, 53]
+    columns = ['UNITID']
+    for i in range(1, 55):
+        if (i not in skipped):
+            columns.append('PCIP{:02d}'.format(i))
+
+    Program_Percentages = all_data_elements[columns]
+
+    Program_Percentages.to_csv('Program_Percentages.csv', index=False, na_rep=r'\N')
+
 
 
 #------------------------ END OF FUNCTION DEFINITIONS --------------------------
@@ -79,7 +115,7 @@ def make_Universities(all_data_elements):
 
 # Set path to IMDb data
 # ---------------------
-data_path = '../'
+data_path = './'
 print('Looking for college data in: ',data_path,'\n')
 
 
@@ -96,3 +132,5 @@ print('Reading Most-Recent-Cohorts-All-Data-Elements.csv')
 all_data_elements = pd.read_csv(os.path.join(data_path, 'Most-Recent-Cohorts-All-Data-Elements.csv'),
                                 na_values='\\N',low_memory=False)
 make_Universities(all_data_elements)
+make_Demographics(all_data_elements)
+make_Program_Percentages(all_data_elements)
